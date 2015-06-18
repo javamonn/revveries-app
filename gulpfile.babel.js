a5ee6aac16f9a2a4eabe87efc1127a323bcbb021
Daniel Ramirez <javamonn@gmail.com>
2015-06-18 00:43:21 -0400

    add deps, setup react, working on build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var babel = require('gulp-babel');
+var gutil = require('gulp-util');
+var browserify = require('browserify');
+var watchify = require('watchify');
+
+gulp.task('js', () => {
+  return gulp
+    .src((['./js/**/*.js'])
+    .pipe(babel())
+    .pipe(
+});
 
 gulp.task('default', () => {
-  console.log('hello, world');
+
 });
