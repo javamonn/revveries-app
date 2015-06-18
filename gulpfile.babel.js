f09872ec59bac3d2cb5d71006818f605e318f5bb
Daniel Ramirez <javamonn@gmail.com>
2015-06-18 01:25:59 -0400

    initial js build task with es6, browserify, uglify, sourcemaps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,27 @@
+'use strict';
+
 var gulp = require('gulp');
-var babel = require('gulp-babel');
 var gutil = require('gulp-util');
 var browserify = require('browserify');
 var watchify = require('watchify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('js', () => {
-  return gulp
-    .src((['./js/**/*.js'])
-    .pipe(babel())
-    .pipe(
+  return watchify(browserify('./js/main.js/', watchify.args))
+    .transform(babelify)
+    .bundle()
+      .on('error', gutil.log)
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+      .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/javascripts/'))
 });
 
 gulp.task('default', () => {
