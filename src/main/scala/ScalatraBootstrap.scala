53dfed85ae25a8c879fe2ebc861e61d22d5b6a1e
Daniel Ramirez <javamonn@gmail.com>
2015-06-23 21:06:48 -0400

    basic slick and postgres setup

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,10 +1,28 @@
 import com.revveries.app._
 import org.scalatra._
 import javax.servlet.ServletContext
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import slick.driver.JdbcDriver.api._
 
 class ScalatraBootstrap extends LifeCycle {
+
+  val cpds = initCpds()
+
   override def init(context: ServletContext) {
+    val db = Database.forDataSources(cpds)
     context.mount(new RevveriesServlet, "/*")
-    context.mount(new GalleryServlet, "/api/gallery")
+    context.mount(new GalleryServlet(db), "/api/gallery/*")
+  }
+
+  override def destroy(context: ServletContext) {
+    cpds.close
+  }
+
+  private def initCpds(): ComboPooledDataSource = {
+    val cpds = new ComboPooledDataSource
+    val dbUri = new URI(sys.env("DATABASE_URL"))
+    cpds.setJbdcUrl("jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath())
+    cpds.setUser(sys.env("DATABASE_USER"))
+    cpds.setPassword(sys.env("DATABASE_PASSWORD"))
   }
 }
