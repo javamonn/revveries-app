3863ad8cddcfa51ef89e7bd2f15b5e76cea5bb84
Daniel Ramirez <javamonn@gmail.com>
2015-07-21 20:12:06 -0400

    reorg

--- src/main/webapp/WEB-INF/js/__tests__/.setupJestTestEnv.js
+++
@@ -1,3 +0,0 @@
-// Setup global mock for fetch polyfill.
-jest.dontMock('../stores/__mocks__/fetch');
-fetch = require('../stores/__mocks__/fetch');
--- src/main/webapp/WEB-INF/js/stores/__tests__/CmsStore.spec.js
+++ src/main/webapp/WEB-INF/js/stores/__tests__/CmsStore.spec.js
@@ -1,4 +1,6 @@
 jest.dontMock('../CmsStore');
+jest.dontMock('../__mocks__/fetch');
+fetch = require('../__mocks__/fetch');
 
 import Reflux from 'reflux';
 import { List } from 'immutable';
