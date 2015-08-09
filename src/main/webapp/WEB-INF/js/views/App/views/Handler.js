3d1448c73c96ff9b127c16ef82c865a291dbf750
Daniel Ramirez <javamonn@gmail.com>
2015-08-08 20:45:45 -0400

    wireframe app component

--- src/main/webapp/WEB-INF/js/views/App/views/Handler.js
+++ src/main/webapp/WEB-INF/js/views/App/views/Handler.js
@@ -2,13 +2,44 @@ import React from 'react';
 import Reflux from 'reflux';
 import Immutable from 'immutable';
 import { RouteHandler, Navigation } from 'react-router';
+import Gallery from 'stores/records/GalleryRecord';
+import Picture from 'stores/records/PictureRecord';
 //import GalleryStore from 'stores/GalleryStore';
 
 var App = React.createClass({
-  //mixins: [
-    //Reflux.listenTo('AppStore', onAppChanged),
-    //Navigation
-  //],
+  mixins: [
+    Navigation,
+    RouteHandler
+  ],
+
+  getInitialState() {
+    var galleries = Immutable.List(
+      initialGalleries.map(gallery => {
+        pictures = Immutable.List(gal.pictures.map(picture => new Picture(picture)));
+        gallery.pictures = pictures;
+        return new Gallery(gallery);
+      })
+    );
+    var defaultPicture = galleries.get(0).pictures.get(0);
+    var galleries = galleries.delete(0);
+    return {
+      galleries,
+      defaultPicture
+    };
+  }, 
+
+  render() {
+    return (
+      <div id="app">
+        <div id="sidenav">
+          
+        </div>
+        <div id="gallery"
+
+        </div>
+      </div>
+    );
+  }
   
 });
 
