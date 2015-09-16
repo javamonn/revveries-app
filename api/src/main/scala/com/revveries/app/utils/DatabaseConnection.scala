d38ba9e4929f5269625fcf63fc650e75fc63d2ed
Daniel Ramirez <javamonn@gmail.com>
2015-09-15 23:07:26 -0400

    responsive landing

--- api/src/main/scala/com/revveries/app/utils/DatabaseConnection.scala
+++ api/src/main/scala/com/revveries/app/utils/DatabaseConnection.scala
@@ -3,11 +3,28 @@ package com.revveries.app.utils
 import org.flywaydb.core.Flyway
 import com.mchange.v2.c3p0.ComboPooledDataSource
 import slick.driver.JdbcDriver.api._
+import scala.util.control.Breaks._
+import org.slf4j.{Logger, LoggerFactory}
 
 class DatabaseConnection(uri: String) {
+
   val flyway = new Flyway
-  flyway.setDataSource(uri, sys.env("POSTGRES_USER"), sys.env("POSTGRES_PASSWORD"))
-  flyway.migrate
+  val logger = LoggerFactory.getLogger(getClass)
+
+  breakable {
+    while (true) {
+      try {
+        flyway.setDataSource(uri, sys.env("POSTGRES_USER"), sys.env("POSTGRES_PASSWORD"))
+        flyway.migrate
+        break
+      } catch {
+        case e: Exception => {
+          logger.info("unable to connect to db, retrying in 100ms")        
+          Thread.sleep(100)
+        }
+      }
+    }
+  }
 
   val cpds: ComboPooledDataSource = new ComboPooledDataSource
   cpds.setJdbcUrl(uri)
