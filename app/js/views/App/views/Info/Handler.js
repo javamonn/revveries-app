87643ed0ce6b605994b158f93d458b8696b7d0b9
Daniel Ramirez <javamonn@gmail.com>
2016-09-07 22:12:21 -0400

    feat: add content to info page, add font-awesome

--- app/js/views/App/views/Info/Handler.js
+++ app/js/views/App/views/Info/Handler.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react'
+import Radium from 'radium'
 
+@Radium
 class Info extends Component {
   render () {
     return (
-      <div>
-      { Array(1000).fill(<span style={{ marginLeft: '4', marginRight: '4'}}>ODETTE CHAVEZ-MAYO</span>) }
+      <div style={styles.base}>
+        <span style={styles.info}>
+          20 yr old photographer living in Yellow Springs, Ohio and studying at Antioch College. I like to take photos.
+        </span>
+        <span style={styles.email}>
+          <a href="mailto:revveries@gmail.com">revveries@gmail.com</a>
+        </span>
+        <div style={styles.socialContainer}>
+          <a href="/" style={styles.socialLink}>
+            <i style={styles.socialIcon} className="fa fa-instagram fa-lg" />
+          </a>
+          <a href="/" style={styles.socialLink}>
+            <i style={styles.socialIcon} className="fa fa-tumblr fa-lg" />
+          </a>
+          <a href="/" style={styles.socialLink}>
+            <i style={styles.socialIcon} className="fa fa-twitter fa-lg" />
+          </a>
+          <a href="/" style={styles.socialLink}>
+            <i style={styles.socialIcon} className="fa fa-facebook fa-lg" />
+          </a>
+        </div>
       </div>
     )
+  }
+}
+
+const styles = {
+  base: {
+    '@media screen and (min-width: 1050px)': {
+      paddingTop: '90',
+    },
+    '@media screen and (max-width: 1050px)': {
+      paddingTop: '30'
+    },
+    paddingLeft: '30',
+    paddingRight: '30'
+  },
+  info: {
+    display: 'block'
+  },
+  email: {
+    display: 'block',
+    marginTop: '16'
+  },
+  socialContainer: {
+    marginTop: '20',
+  },
+  socialIcon: {
+    marginRight: '18'
+  },
+  socialLink: {
+    textDecoration: 'none',
+    color: '#000'
   }
 }
 
